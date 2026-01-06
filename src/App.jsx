7acaf01e834e36e1b32922921110689090717381
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-06 21:45:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl text-red-600 font-bold">
+        HELLO WORLD 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
