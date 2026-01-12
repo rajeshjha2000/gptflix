bbfcc2f75fb8a50354822cb2b87b8195699c0786
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-12 22:38:48 +0530

    added TMDB APIs for nowPlayingMovies with access token

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 const Browse = () => {
+ const getNowPlayingMovies = async () => {
+ const data = await fetch ("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
+ const json = await data.json();
+ };
 
+
+ useEffect(()=>{
+   getNowPlayingMovies();
+ },[])
   return (
     <div>
       <Header />
