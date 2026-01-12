852369ac5bfad1cf65a0cc650ca1945777d703d3
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-12 13:20:36 +0530

    added constant file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 
 const Header = () => {
@@ -44,7 +45,7 @@ const Header = () => {
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
-      <img className="w-44 mx-auto md:mx-0" src={""} alt="logo" />
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
           <img
             className="hidden md:block w-12 h-12"
             alt="usericon"
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,6 +9,7 @@ import {
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 
 const Login = () => {
@@ -36,7 +37,7 @@ const Login = () => {
           const user = userCredential.user;
           updateProfile(user, {
             displayName: name.current.value,
-            photoURL: "user avatar",
+            photoURL: USER_AVATAR,
           })
             .then(() => {
               const { uid, email, displayName, photoURL } = auth.currentUser;
@@ -84,7 +85,7 @@ const Login = () => {
     <div>
       <Header />
       <div className="absolute">
-        <img className="h-screen object-cover" src={..} alt="logo" />
+        <img className="h-screen object-cover" src={BG_URL} alt="logo" />
       </div>
       <form
         onSubmit={(e) => e.preventDefault()}
