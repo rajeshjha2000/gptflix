4d8ffc4a14220fe6b0926561de8f4384cbaa3919
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-12 16:18:00 +0530

    typo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,17 +6,15 @@ import { auth } from "../utils/firebase";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
 
-
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+
   const handleSignOut = () => {
-    signOut(auth)
-      .then(() => {})
-      .catch((error) => {
-        navigate("/error");
-      });
+    signOut(auth).catch(() => {
+      navigate("/error");
+    });
   };
 
   useEffect(() => {
@@ -25,10 +23,10 @@ const Header = () => {
         const { uid, email, displayName, photoURL } = user;
         dispatch(
           addUser({
-            uid: uid,
-            email: email,
-            displayName: displayName,
-            photoURL: photoURL,
+            uid,
+            email,
+            displayName,
+            photoURL,
           })
         );
         navigate("/browse");
@@ -38,25 +36,30 @@ const Header = () => {
       }
     });
 
-    // Unsiubscribe when component unmounts
     return () => unsubscribe();
-  }, []);
-
+  }, [dispatch, navigate]);
 
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+
+      {user && (
+        <div className="flex items-center gap-4">
           <img
-            className="hidden md:block w-12 h-12"
+            className="hidden md:block w-12 h-12 rounded"
             alt="usericon"
-            src={user?.photoURL}
+            src={user.photoURL}
           />
-          <button onClick={handleSignOut} className="font-bold text-white ">
-            (Sign Out)
+          <button
+            onClick={handleSignOut}
+            className="font-bold text-white"
+          >
+            Sign Out
           </button>
         </div>
       )}
     </div>
   );
 };
+
 export default Header;
