979dcb9febe3fb1fa5e8a9b76ab6ea41b33c5175
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-12 22:56:14 +0530

    added nowPlayingMovies data into redux store

--- src/Components/Browse.jsx
+++ src/Components/Browse.jsx
@@ -1,11 +1,16 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovies } from "../utils/moviesSlice";
 
 const Browse = () => {
+ const dispatch = useDispatch();
  const getNowPlayingMovies = async () => {
  const data = await fetch ("https://api.themoviedb.org/3/movie/now_playing?page=1", API_OPTIONS);
  const json = await data.json();
+
+ dispatch(addNowPlayingMovies(json.results));
  };
 
 
