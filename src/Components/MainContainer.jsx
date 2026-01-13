7da7e644195ef9c7f1f301e72a7d8b6eaa93f734
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2026-01-13 16:58:59 +0530

    developed main container

--- src/Components/MainContainer.jsx
+++ src/Components/MainContainer.jsx
@@ -9,11 +9,12 @@ const MainContainer = () => {
 
   const mainMovie = movies[0];
 
+  const { original_title, overview, id } = mainMovie;
 
   return (
-    <div>
-      <VideoTitle/>
-      <VideoBackground/>
+    <div className="pt-[30%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
